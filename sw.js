d3bf78bde48ace2c0f04fe5b43f9794bc3c7b12f
Eljas-Webstar <elja500@icloud.com>
2026-09-15 18:41:12 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+/* Setterboard: Servicearbeiter für Push und Homescreen */
+self.addEventListener("install", function(e){ self.skipWaiting(); });
+self.addEventListener("activate", function(e){ e.waitUntil(self.clients.claim()); });
+
+self.addEventListener("push", function(e){
+  var d = {};
+  try { d = e.data ? e.data.json() : {}; } catch(err){ d = { titel:"Setterboard", text: e.data ? e.data.text() : "" }; }
+  var titel = d.titel || "Setterboard";
+  var opt = {
+    body: d.text || "",
+    icon: "icon-192.png",
+    badge: "icon-192.png",
+    tag: d.tag || undefined,
+    renotify: !!d.tag,
+    data: { termin: d.termin || "", pfad: d.pfad || "./" }
+  };
+  e.waitUntil(self.registration.showNotification(titel, opt));
+});
+
+self.addEventListener("notificationclick", function(e){
+  e.notification.close();
+  var id = (e.notification.data && e.notification.data.termin) || "";
+  var ziel = new URL("./" + (id ? ("#termin=" + id) : ""), self.location.href).href;
+  e.waitUntil(
+    self.clients.matchAll({ type:"window", includeUncontrolled:true }).then(function(liste){
+      for (var i=0; i<liste.length; i++){
+        var c = liste[i];
+        if (c.url.indexOf(self.registration.scope) === 0 && "focus" in c){
+          c.postMessage({ art:"termin-oeffnen", id:id });
+          return c.focus();
+        }
+      }
+      return self.clients.openWindow(ziel);
+    })
+  );
+});
